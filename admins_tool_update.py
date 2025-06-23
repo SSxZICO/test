d41266ad18f0e432bca5bce4c0706a8097b505f3
SSxZICO <144567454+SSxZICO@users.noreply.github.com>
2025-06-24 01:21:48 +0300

    Update admins_tool_update.py

--- admins_tool_update.py
+++ admins_tool_update.py
@@ -1,6 +1,6 @@
 #    Friendly Telegram (telegram userbot)
 #    Copyright (C) 2018-2019 The Authors
-#    update module dev: @xduko
+#    update module dev: @xduko 2025:06:24
 
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -17,6 +17,8 @@
 
 from .. import loader, utils, security
 import logging
+from datetime import timedelta, datetime
+import re
 
 from telethon.tl.types import ChatAdminRights, ChatBannedRights, PeerUser, PeerChannel
 from telethon.errors import BadRequestError
@@ -27,7 +29,8 @@ logger = logging.getLogger(__name__)
 
 @loader.tds
 class BanMod(loader.Module):
-    """Админтулс"""
+    """Админтулс — ban, unban, kick, promote, demote, mute, unmute\nupdate module dev: @xduko"""
+
     strings = {
         "name": "AdminTools",
         "not_supergroup": "<b>Это не супергруппа!</b>",
@@ -37,104 +40,109 @@ class BanMod(loader.Module):
         "kick_none": "<b>Кто хочет принудительно покинуть чат?</b>",
         "promote_none": "<b>Кто хочет опку?</b>",
         "demote_none": "<b>Укажи с кого снять админку?</b>",
+        "mute_none": "<b>Кого замьютить?</b>",
+        "unmute_none": "<b>Кого размьютить?</b>",
         "who": "<b>Кого...?</b>",
         "not_admin": "<b>Я не администратор...</b>",
-        "banned": "<code>{}</code> <b>Получил бан!</b>\n<b>ID:</b> <code>{}</code>",
-        "unbanned": "<code>{}</code> <b>Получил разбан!</b>\n<b>ID:</b> <code>{}</code>",
-        "kicked": "<code>{}</code> <b>Был кикнут!</b>\n<b>ID:</b> <code>{}</code>",
-        "promoted": "<code>{}</code> <b>Получил права администратора!</b>\n<b>ID:</b> <code>{}</code>",
-        "demoted": "<code>{}</code> <b>Потерял права администратора!</b>\n<b>ID:</b> <code>{}</code>"
+        "banned": "<code>{}</code> <b>получил бан!</b> ID: <code>{}</code>",
+        "unbanned": "<code>{}</code> <b>разбанен!</b> ID: <code>{}</code>",
+        "kicked": "<code>{}</code> <b>кикнут!</b> ID: <code>{}</code>",
+        "promoted": "<code>{}</code> <b>стал админом!</b> ID: <code>{}</code>",
+        "demoted": "<code>{}</code> <b>лишён админки!</b> ID: <code>{}</code>",
+        "muted": "<code>{}</code> <b>замьючен на {}</b> ID: <code>{}</code>",
+        "unmuted": "<code>{}</code> <b>размьючен</b> ID: <code>{}</code>"
     }
 
     async def client_ready(self, client, db):
         self.client = client
 
+    def _parse_time(self, text):
+        match = re.fullmatch(r"(\d+)([smhdwM])", text)
+        if not match:
+            return None
+        num, unit = int(match[1]), match[2]
+        return timedelta(seconds=num) if unit == "s" else \
+               timedelta(minutes=num) if unit == "m" else \
+               timedelta(hours=num) if unit == "h" else \
+               timedelta(days=num) if unit == "d" else \
+               timedelta(weeks=num) if unit == "w" else \
+               timedelta(days=30*num) if unit == "M" else None
+
+    async def _get_user(self, message, none_text):
+        if message.is_reply:
+            reply = await message.get_reply_message()
+            if not reply:
+                await utils.answer(message, none_text)
+                return None
+            return await utils.get_user(reply)
+        args = utils.get_args(message)
+        if not args:
+            await utils.answer(message, none_text)
+            return None
+        who = int(args[0]) if args[0].isdigit() else args[0]
+        try:
+            return await self.client.get_entity(who)
+        except Exception:
+            await utils.answer(message, self.strings("who", message))
+            return None
+
     @loader.group_admin_ban_users
-    @loader.ratelimit
     async def bancmd(self, message):
         """Бан в чате"""
-        if not isinstance(message.to_id, PeerChannel):
-            return await utils.answer(message, self.strings("not_supergroup", message))
-
         user = await self._get_user(message, self.strings("ban_none", message))
         if not user:
             return
-
         try:
-            await self.client(EditBannedRequest(message.chat_id, user.id, ChatBannedRights(view_messages=True)))
+            await self.client(EditBannedRequest(message.chat_id, user.id,
+                ChatBannedRights(until_date=None, view_messages=True)))
         except BadRequestError:
             return await utils.answer(message, self.strings("not_admin", message))
-
         await self.allmodules.log("ban", group=message.chat_id, affected_uids=[user.id])
         await utils.answer(message, self.strings("banned", message).format(utils.escape_html(user.first_name), user.id))
 
     @loader.group_admin_ban_users
-    @loader.ratelimit
     async def unbancmd(self, message):
         """Разбан в чате"""
-        if not isinstance(message.to_id, PeerChannel):
-            return await utils.answer(message, self.strings("not_supergroup", message))
-
         user = await self._get_user(message, self.strings("unban_none", message))
         if not user:
             return
-
         try:
-            await self.client(EditBannedRequest(message.chat_id, user.id, ChatBannedRights(view_messages=False)))
+            await self.client(EditBannedRequest(message.chat_id, user.id,
+                ChatBannedRights(until_date=None, view_messages=False)))
         except BadRequestError:
             return await utils.answer(message, self.strings("not_admin", message))
-
         await self.allmodules.log("unban", group=message.chat_id, affected_uids=[user.id])
         await utils.answer(message, self.strings("unbanned", message).format(utils.escape_html(user.first_name), user.id))
 
     @loader.group_admin_ban_users
-    @loader.ratelimit
     async def kickcmd(self, message):
         """Кикнуть из чата"""
-        if isinstance(message.to_id, PeerUser):
-            return await utils.answer(message, self.strings("not_group", message))
-
         user = await self._get_user(message, self.strings("kick_none", message))
         if not user:
             return
-
         try:
             await self.client.kick_participant(message.chat_id, user.id)
         except BadRequestError:
             return await utils.answer(message, self.strings("not_admin", message))
-
         await self.allmodules.log("kick", group=message.chat_id, affected_uids=[user.id])
         await utils.answer(message, self.strings("kicked", message).format(utils.escape_html(user.first_name), user.id))
 
     @loader.group_admin_add_admins
-    @loader.ratelimit
     async def promotecmd(self, message):
         """Дать админку"""
         args = utils.get_args(message)
         user = await self._get_user(message, self.strings("promote_none", message))
         if not user:
             return
-
         rank = ' '.join(args[1:]) if len(args) > 1 else ""
-
         rights = ChatAdminRights(
-            change_info=True,
-            post_messages=True,
-            edit_messages=True,
-            delete_messages=True,
-            ban_users=True,
-            invite_users=True,
-            pin_messages=True,
-            add_admins=False,
-            manage_call=True,
-            anonymous=False,
-        )
-
+            change_info=True, post_messages=True, edit_messages=True,
+            delete_messages=True, ban_users=True, invite_users=True,
+            pin_messages=True, add_admins=False, anonymous=False, manage_call=True)
         try:
             await self.client(EditAdminRequest(message.chat_id, user.id, rights, rank))
         except BadRequestError:
             return await utils.answer(message, self.strings("not_admin", message))
-
         await self.allmodules.log("promote", group=message.chat_id, affected_uids=[user.id])
         await utils.answer(message, self.strings("promoted", message).format(utils.escape_html(user.first_name), user.id))
 
@@ -144,46 +152,44 @@ class BanMod(loader.Module):
         user = await self._get_user(message, self.strings("demote_none", message))
         if not user:
             return
-
         try:
-            if message.is_channel:
-                rights = ChatAdminRights(
-                    change_info=False,
-                    post_messages=False,
-                    edit_messages=False,
-                    delete_messages=False,
-                    ban_users=False,
-                    invite_users=False,
-                    pin_messages=False,
-                    add_admins=False,
-                    manage_call=False,
-                    anonymous=False,
-                )
-                await self.client(EditAdminRequest(message.chat_id, user.id, rights, ""))
-            else:
-                await self.client(EditChatAdminRequest(message.chat_id, user.id, False))
+            await self.client(EditAdminRequest(message.chat_id, user.id,
+                ChatAdminRights(False, False, False, False, False, False, False, False, False), ""))
         except BadRequestError:
-            return await utils.answer(message, self.strings("not_admin", message))
-
+            try:
+                await self.client(EditChatAdminRequest(message.chat_id, user.id, False))
+            except:
+                return await utils.answer(message, self.strings("not_admin", message))
         await self.allmodules.log("demote", group=message.chat_id, affected_uids=[user.id])
         await utils.answer(message, self.strings("demoted", message).format(utils.escape_html(user.first_name), user.id))
 
-    async def _get_user(self, message, empty_error_text):
-        if message.is_reply:
-            reply = await message.get_reply_message()
-            if not reply:
-                await utils.answer(message, empty_error_text)
-                return None
-            return await utils.get_user(reply)
-
+    @loader.group_admin_ban_users
+    async def mutecmd(self, message):
+        """Замьютить пользователя. Используй: .mute [реплай или id] [время: 10m, 2h, 7d, 1M]"""
         args = utils.get_args(message)
-        if not args:
-            await utils.answer(message, empty_error_text)
-            return None
-
-        who = int(args[0]) if args[0].isdigit() else args[0]
+        user = await self._get_user(message, self.strings("mute_none", message))
+        if not user:
+            return
+        delta = self._parse_time(args[1]) if len(args) > 1 else timedelta(days=365)
+        until = datetime.utcnow() + delta
         try:
-            return await self.client.get_entity(who)
-        except Exception:
-            await utils.answer(message, self.strings("who", message))
-            return None
+            await self.client(EditBannedRequest(message.chat_id, user.id,
+                ChatBannedRights(until_date=until, send_messages=True)))
+        except BadRequestError:
+            return await utils.answer(message, self.strings("not_admin", message))
+        await self.allmodules.log("mute", group=message.chat_id, affected_uids=[user.id])
+        await utils.answer(message, self.strings("muted", message).format(utils.escape_html(user.first_name), args[1] if len(args) > 1 else "навсегда", user.id))
+
+    @loader.group_admin_ban_users
+    async def unmutecmd(self, message):
+        """Размьютить пользователя"""
+        user = await self._get_user(message, self.strings("unmute_none", message))
+        if not user:
+            return
+        try:
+            await self.client(EditBannedRequest(message.chat_id, user.id,
+                ChatBannedRights(until_date=None, send_messages=False)))
+        except BadRequestError:
+            return await utils.answer(message, self.strings("not_admin", message))
+        await self.allmodules.log("unmute", group=message.chat_id, affected_uids=[user.id])
+        await utils.answer(message, self.strings("unmuted", message).format(utils.escape_html(user.first_name), user.id))
